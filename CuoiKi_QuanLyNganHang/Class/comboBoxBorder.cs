93fe1b3e0e6ca049b6fdeb1014ece20582e55587
quanghlmtl <93662043+quanghlmtl@users.noreply.github.com>
2023-10-29 14:48:41 +0700

    upadate

---
+++ CuoiKi_QuanLyNganHang/Class/comboBoxBorder.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using CuoiKi_QuanLyNganHang.Properties;
+using System.Drawing.Drawing2D;
+using System.ComponentModel;
+
+namespace CuoiKi_QuanLyNganHang.Class
+{
+    public class comboBoxBorder : ComboBox
+    {
+
+        private Color borderColor = Color.MediumSlateBlue;
+        private int borderSize = 2;
+        private bool underLinedStyle = false;
+        public comboBoxBorder()
+        {
+            this.Font = new Font("Averta Std CY", 10f);
+            this.FlatStyle = FlatStyle.Flat;
+            this.Size = new Size(100, 20);
+            this.ForeColor = Color.White;
+            this.underLinedStyle = true;
+            this.Padding = new Padding(7,7,7,7);
+        }
+        [Category("Custom Advance")]
+        public Color BorderColor
+        {
+            get
+            {
+                return borderColor;
+            }
+            set
+            {
+                borderColor = value;
+                this.Invalidate();
+            }
+        }
+        [Category("Custom Advance")]
+        public int BorderSize
+        {
+            get
+            {
+                return borderSize;
+            }
+            set
+            {
+                borderSize = value;
+                this.Invalidate();
+            }
+        }
+        [Category("Custom Advance")]
+        public bool UnderLinedStyle
+        {
+            get
+            {
+                return underLinedStyle;
+            }
+            set
+            {
+                underLinedStyle = value;
+                this.Invalidate();
+            }
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics graph = e.Graphics;
+
+            //Draw border
+            using (Pen penBorder = new Pen(borderColor, borderSize))
+            {
+                penBorder.Alignment = System.Drawing.Drawing2D.PenAlignment.Inset;
+                if (underLinedStyle)//line style
+                    graph.DrawLine(penBorder, 0, this.Height - 1, this.Width, this.Height - 1);
+                else//normal style
+                    graph.DrawRectangle(penBorder, 0, 0, this.Width - 0.5F, this.Height - 0.5F);
+            }
+        }
+    }
+}
